d0acb52fb0d9ca6f334e1fec00b979cf8d84b836
Anuj Rathod <96101044+Anuj2731@users.noreply.github.com>
2025-01-30 15:47:24 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+import sys 
+from PyQt6.QtWidgets import QApplication, QMessageBox
+from database import init_db
+from app import ExpenseApp
+
+def main():
+    app = QApplication(sys.argv)
+
+    if not init_db("expense.db"):
+        QMessageBox.critical(None, "Error", "Could not load your Database...")
+    window = ExpenseApp()
+    window.show()
+
+    sys.exit(app.exec())
+
+if __name__=="__main__":
+    main()
